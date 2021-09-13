a13c841737f3029de8361af12681c6a14d248627
Ikbal <ikbal.malik26@gmail.com>
2021-09-13 12:02:00 +0700

    fix meta description

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Wilio Survey, Quotation, Review and Register form Wizard by Ansonika.">
+	<meta name="description" content="Open Member Tahungoding 2021">
 	<meta name="author" content="Ansonika">
 	<title>Recruitment | Tahungoding</title>
 	<!-- Favicons-->
