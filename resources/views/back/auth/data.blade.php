ede93b6eb444cbac081d610c13acbd5cce304040
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2024-09-15 20:42:36 +0700

    allow-mixed-content

--- resources/views/back/auth/data.blade.php
+++ resources/views/back/auth/data.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
